ecb4fde34d76b7c5672f49216dee4e0dfea9e86d
Manasse Daniel <danidkm242@gmail.com>
2023-05-05 15:36:10 +0100

    adding loading effect

--- src/app/pokemon/loader/loader.component.ts
+++ src/app/pokemon/loader/loader.component.ts
@@ -3,12 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-loader',
   template: `
-    <p>
-      loader works!
-    </p>
+    <div class="preloader-wrapper big active">
+      <div class="spinner-layer spinner-blue">
+        <div class="circle-clipper left">
+          <div class="circle"></div>
+        </div><div class="gap-patch">
+          <div class="circle"></div>
+        </div><div class="circle-clipper right">
+          <div class="circle"></div>
+        </div>
+      </div>
+    </div>
   `,
-  styles: [
-  ]
+ 
 })
 export class LoaderComponent {
 
